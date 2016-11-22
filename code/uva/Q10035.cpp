027245a73bd655f8f1d382ecf3ff0f61c76f2143
Siao,Yu-Jie <a6194315535@gmail.com>
2016-11-22 21:04:01 +0800

    Update Q10035.cpp
    Solved Q10035

--- code/uva/Q10035.cpp
+++ code/uva/Q10035.cpp
@@ -1,17 +1,17 @@
-//Not Solved Yet
-
 #include <iostream>
+#define LL long long int
 using namespace std;
-
 int main() {
-	int a,b;
+	LL a,b;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 	while(cin>>a>>b){
-		int carry,tmp,sum=0;
-		int tmpa[15]={0},tmpb[15]={0};
 		if(a==0&&b==0)return 0;
+		int sum=0,carry=0;
 		while(a!=0||b!=0){
-			sum = (a % 10 + b % 10) / 10 + sum;
-			a/=10;b/=10;
+			sum += (a%10 + b%10 + carry) / 10;
+			carry = (a%10 + b%10 + carry) / 10;
+			a /= 10;b /= 10;
 		}
 		if(sum==0)cout<<"No carry operation.\n";
 		else if(sum==1)cout<<"1 carry operation.\n";
