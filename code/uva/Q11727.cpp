bd42cca257d66928b8f9035a9e4ca2059a241d88
Siao,Yu-Jie <a6194315535@gmail.com>
2016-11-23 22:26:15 +0800

    Create Q11727.cpp

---
+++ code/uva/Q11727.cpp
@@ -0,0 +1,14 @@
+//就懶得打一堆 if 所以直接用 sort
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+	int tmp[3],n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>tmp[0]>>tmp[1]>>tmp[2];
+		sort(tmp,tmp+3);
+		cout<<"Case "<<i<<": "<<tmp[1]<<'\n';
+	}
+	return 0;
+}
