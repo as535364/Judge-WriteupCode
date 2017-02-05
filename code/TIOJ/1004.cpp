639b985983dc7fc8525b4d668f1060429e9db99b
Siao,Yu-Jie <a6194315535@gmail.com>
2017-02-05 11:15:57 +0800

    Update 1004.cpp

--- code/TIOJ/1004.cpp
+++ code/TIOJ/1004.cpp
@@ -6,22 +6,22 @@ int main(){
     int n=10;//n 是人數
     while(cin>>n){
         int num[55]={1},list[55]={0};//num 是紀錄用 //list 為死亡順序 index 從一開始
-		int j=0,number=0,s=0,t;//number 是死亡人數，j 為報數用
-		for (int i=1;i<=n;i++)num[i]=1;//重置狀態 1 為活者
+	int j=0,number=0,s=0,t;//number 是死亡人數，j 為報數用
+	for (int i=1;i<=n;i++)num[i]=1;//重置狀態 1 為活者
         t=n;
-		while (number<n){
+	while (number<n){
             for (int i=1;i<=n;i++){
                 if (num[i]==1){
                     j++;
                     if (j==2){//滿足自殺報數
                         num[i]=0;
-						list[++s]=i;                        
+			list[++s]=i;                        
                         j=0;
                         number++;
                     }
                 }
             }
         }
-		cout<<list[t]<<endl;
+	cout<<list[t]<<endl;
     }
 }
